dc1e9d51333a383c6e52901e749ce1638806e48d
textile86 <textile2006@list.ru>
2025-11-23 16:59:31 +0300

    dynamic-routes

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -6,15 +6,17 @@ public class HelloWorld {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-
-        app.get("/hello", ctx -> {
-            String name = ctx.queryParam("name");git
-            if (name == null) {
-                name = "World";
-            }
-            String greeting = "Hello " + name + "!";
-            ctx.result(greeting);
+        app.get("/courses/{id}", ctx -> {
+            ctx.result("Cource ID: " + ctx.pathParam("id"));
         });
-        app.start(7070);
+        app.get("/users/{id}", ctx -> {
+            ctx.result("Users ID: " + ctx.pathParam("id"));
+        });
+        app.get("/users/{id}/posts/{postId}", ctx -> {
+            String userId = ctx.pathParam("id");
+            String postId = ctx.pathParam("postId");
+            ctx.result("Course ID: " + userId + " Post ID: " + postId);
+        });
+        app.start(7071);
     }
 }
