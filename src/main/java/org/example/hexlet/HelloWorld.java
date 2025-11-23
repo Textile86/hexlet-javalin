5c27b30457aff76c4a262687c2822ce5b1b32fee
textile86 <textile2006@list.ru>
2025-11-23 13:32:17 +0300

    Http session

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,15 +1,20 @@
 package org.example.hexlet;
-
 import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParam("name");git
+            if (name == null) {
+                name = "World";
+            }
+            String greeting = "Hello " + name + "!";
+            ctx.result(greeting);
+        });
+        app.start(7070);
     }
 }
