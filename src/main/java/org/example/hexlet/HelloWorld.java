a43676b2a30f02f6b9c3e2635cc998bcc8ece975
textile86 <textile2006@list.ru>
2025-11-28 17:31:40 +0300

    validation

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -3,8 +3,12 @@ package org.example.hexlet;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import static io.javalin.rendering.template.TemplateUtil.model;
+
+import io.javalin.validation.ValidationException;
 import org.apache.commons.text.StringEscapeUtils;
 
+import org.example.hexlet.dto.courses.BuildCoursePage;
+import org.example.hexlet.dto.users.BuildUserPage;
 import org.example.hexlet.model.Course;
 import org.example.hexlet.dto.courses.CoursesPage;
 import org.example.hexlet.dto.courses.CoursePage;
@@ -13,7 +17,6 @@ import org.example.hexlet.repository.CourseRepository;
 import org.example.hexlet.repository.UserRepository;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class HelloWorld {
@@ -32,16 +35,27 @@ public class HelloWorld {
         app.get("/", ctx -> ctx.render("index.jte"));
 
         app.get("/courses/build", ctx -> {
-            ctx.render("courses/build.jte");
+            var page = new BuildCoursePage();
+            ctx.render("courses/build.jte", model("page", page));
         });
 
         app.post("/courses", ctx -> {
-            var name = ctx.formParam("name").trim();
-            var description = ctx.formParam("description").trim();
-
-            var course = new Course(name, description);
-            CourseRepository.save(course);
-            ctx.redirect("/courses");
+            String name = null;
+            String description = null;
+            try {
+                name = ctx.formParamAsClass("name", String.class)
+                        .check(value -> value.length() > 2, "Имя должно быть длиннее 2х символов")
+                        .get();
+                description = ctx.formParamAsClass("description", String.class)
+                        .check(value -> value.length() > 10, "Описание должно быть длиннее 10ти символов")
+                        .get();
+                var course = new Course(name, description);
+                CourseRepository.save(course);
+                ctx.redirect("/courses");
+            } catch (ValidationException e) {
+                var page = new BuildCoursePage(name, description, e.getErrors());
+                ctx.render("courses/build.jte", model("page", page));
+            }
         });
 
         app.get("/courses", ctx -> {
@@ -84,18 +98,26 @@ public class HelloWorld {
         });
 
         app.get("/users/build", ctx -> {
-            ctx.render("users/build.jte");
+            var page = new BuildUserPage();
+            ctx.render("users/build.jte", model("page", page));
         });
 
         app.post("/users", ctx -> {
-            var name = ctx.formParam("name").trim();
-            var email = ctx.formParam("email").trim().toLowerCase();
-            var password = ctx.formParam("password");
-            var passwordConfirmation = ctx.formParam("passwordConfirmation");
+            var name = ctx.formParam("name");
+            var email = ctx.formParam("email");
 
-            var user = new User(name, email, password);
-            UserRepository.save(user);
-            ctx.redirect("/users");
+            try {
+                var passwordConfirmation = ctx.formParam("passwordConfirmation");
+                var password = ctx.formParamAsClass("password", String.class)
+                        .check(value -> value.equals(passwordConfirmation), "Пароли не совпадают")
+                        .get();
+                var user = new User(name, email, password);
+                UserRepository.save(user);
+                ctx.redirect("/users");
+            } catch (ValidationException e) {
+                var page = new BuildUserPage(name, email, e.getErrors());
+                ctx.render("users/build.jte", model("page", page));
+            }
         });
 
         app.get("/users", ctx -> {
@@ -103,7 +125,7 @@ public class HelloWorld {
             ctx.render("users/index.jte", model("users", users));
         });
 
-        app.get("/test-escape", ctx -> {
+            app.get("/test-escape", ctx -> {
             String dangerousInput = "<script>alert('XSS')</script><div>Normal text</div>";
             String safeOutput = StringEscapeUtils.escapeHtml4(dangerousInput);
 
